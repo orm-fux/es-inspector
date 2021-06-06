ad2d2ed24ef201d0e2d4e6225cb1ce0cf83bb835
Orm Fux <46991751+orm-fux@users.noreply.github.com>
2021-06-06 09:46:38 +0200

    Showing progress dialog during session restore

--- src/main/java/com/github/ormfux/esi/ESInspectorApplication.java
+++ src/main/java/com/github/ormfux/esi/ESInspectorApplication.java
@@ -3,6 +3,7 @@ package com.github.ormfux.esi;
 import static com.github.ormfux.simple.di.InjectionContext.getBean;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 
 import com.github.ormfux.esi.controller.AliasDetailsController;
@@ -35,10 +36,20 @@ import com.github.ormfux.esi.ui.images.ImageRegistry;
 import com.github.ormfux.esi.ui.index.IndexDetailsTab;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 public class ESInspectorApplication extends Application {
 
@@ -79,14 +90,16 @@ public class ESInspectorApplication extends Application {
         
         final Scene scene = new Scene(new MainLayout(connectionsListView, mainTabs, loggingView), 1280, 720);
         scene.getStylesheets().add(ESInspectorApplication.class.getResource("ui/es-inspector.css").toExternalForm());
-        primaryStage.setScene(scene);
-        primaryStage.show();
         
-        restorePreviousSession();
+        primaryStage.setOnShown(e -> restorePreviousSession(scene.getWindow()));
+        
         primaryStage.setOnCloseRequest(e -> {
             saveCurrentSession();
             System.exit(1);
         });
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
     private void saveCurrentSession() {
@@ -104,46 +117,99 @@ public class ESInspectorApplication extends Application {
         sessionService.saveSession(currentSession);
     }
     
-    private void restorePreviousSession() {
+    private void restorePreviousSession(final Window parentWindow) {
         loggingService.addLogEntry(Level.INFO, "Restoring previous session.");
         final InspectorSession session = sessionService.loadSession();
         
-        if (session.getTabData() != null) {
-            for (final SessionTabData tabData : session.getTabData()) {
-                if (tabData instanceof SessionAliasDetailsTabData) {
-                    final SessionAliasDetailsTabData aliasTabData = (SessionAliasDetailsTabData) tabData;
-                    final ESMultiIndexAlias alias = sessionService.lookupAlias(aliasTabData);
+        if (session.getTabData() != null && !session.getTabData().isEmpty()) {
+            final Stage progressDialog = new Stage(StageStyle.UNDECORATED);
+            progressDialog.initModality(Modality.WINDOW_MODAL);
+            progressDialog.setResizable(false);
+            progressDialog.initOwner(parentWindow);
+            
+            final Task<Void> restoreTask = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    int progressCount = 0;
+                    updateProgress(progressCount++, session.getTabData().size());
                     
-                    if (alias != null) {
-                        final AliasDetailsTab tab = openAliasDetailsTab(alias);
-                        tab.fillWithRestoreData(aliasTabData);
-                    } else {
-                        loggingService.addLogEntry(Level.WARN, "Could not find connection or alias for alias name: " + aliasTabData.getAliasName());
+                    for (final SessionTabData tabData : session.getTabData()) {
+                        final CountDownLatch doneLatch = new CountDownLatch(1);
+                        updateProgress(progressCount++, session.getTabData().size());
+                        
+                        Platform.runLater(() -> {
+                            try {
+                                restoreTab(tabData);
+                            } finally {
+                                doneLatch.countDown();
+                            }
+                        });
+                        
+                        doneLatch.await();
                     }
                     
-                } else if (tabData instanceof SessionIndexDetailsTabData) {
-                    final SessionIndexDetailsTabData indexTabData = (SessionIndexDetailsTabData) tabData;
-                    final ESIndex index = sessionService.lookupIndex(indexTabData);
-                    
-                    if (index != null) {
-                        final IndexDetailsTab tab = openIndexDetailsTab(index);
-                        tab.fillWithRestoreData(indexTabData);
-                    } else {
-                        loggingService.addLogEntry(Level.WARN, "Could not find connection or index for index name: " + indexTabData.getIndexName());
-                    }
-
-                    
-                } else if (tabData instanceof SessionGMTabData) {
-                    final SessionGMTabData godModeTabData = (SessionGMTabData) tabData;
-                    final ESConnection connection = sessionService.lookupConnection(godModeTabData);
-                    
-                    if (connection != null) {
-                        final GodModeTab tab = openGodModeTab(connection);
-                        tab.fillWithRestoreData(godModeTabData);
-                    } else {
-                        loggingService.addLogEntry(Level.WARN, "Could not find connection for god mode tab.");
-                    }
+                    return null;
                 }
+            };
+            
+            restoreTask.stateProperty().addListener((prop, oldState, newState) -> {
+                if (newState == Worker.State.SUCCEEDED || newState == Worker.State.CANCELLED || newState == Worker.State.FAILED) {
+                    progressDialog.close();
+                }
+            });
+            
+            restoreTask.exceptionProperty().addListener((prop, oldState, newState) -> {
+                loggingService.addLogEntry(newState);
+            });
+            
+            final Text restoringLabel = new Text("Restoring pinned tabs");
+            final ProgressBar progressBar = new ProgressBar();
+            progressBar.progressProperty().bind(restoreTask.progressProperty());
+            
+            final VBox progressDialogContent = new VBox(3);
+            progressDialogContent.setAlignment(Pos.CENTER);
+            progressDialogContent.getChildren().addAll(restoringLabel, progressBar);
+            
+            progressDialog.setScene(new Scene(progressDialogContent));
+            progressDialog.show();
+            
+            new Thread(restoreTask).start();
+        }
+    }
+
+    private void restoreTab(final SessionTabData tabData) {
+        if (tabData instanceof SessionAliasDetailsTabData) {
+            final SessionAliasDetailsTabData aliasTabData = (SessionAliasDetailsTabData) tabData;
+            final ESMultiIndexAlias alias = sessionService.lookupAlias(aliasTabData);
+            
+            if (alias != null) {
+                final AliasDetailsTab tab = openAliasDetailsTab(alias);
+                tab.fillWithRestoreData(aliasTabData);
+            } else {
+                loggingService.addLogEntry(Level.WARN, "Could not find connection or alias for alias name: " + aliasTabData.getAliasName());
+            }
+            
+        } else if (tabData instanceof SessionIndexDetailsTabData) {
+            final SessionIndexDetailsTabData indexTabData = (SessionIndexDetailsTabData) tabData;
+            final ESIndex index = sessionService.lookupIndex(indexTabData);
+            
+            if (index != null) {
+                final IndexDetailsTab tab = openIndexDetailsTab(index);
+                tab.fillWithRestoreData(indexTabData);
+            } else {
+                loggingService.addLogEntry(Level.WARN, "Could not find connection or index for index name: " + indexTabData.getIndexName());
+            }
+
+            
+        } else if (tabData instanceof SessionGMTabData) {
+            final SessionGMTabData godModeTabData = (SessionGMTabData) tabData;
+            final ESConnection connection = sessionService.lookupConnection(godModeTabData);
+            
+            if (connection != null) {
+                final GodModeTab tab = openGodModeTab(connection);
+                tab.fillWithRestoreData(godModeTabData);
+            } else {
+                loggingService.addLogEntry(Level.WARN, "Could not find connection for god mode tab.");
             }
         }
     }
